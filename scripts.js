1f40475eb5624503cc398fc78ab1254726bbefd6
CheePohHian <cheepohhian@gmail.com>
2022-02-24 01:30:10 +0800

    Update 24 February
    Design Testing

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const selectElement = (element) => document.querySelector(element);
-
-// Open and close menubar on click, change nav elements
-selectElement('.menu') .addEventListener('click', () => {
-    selectElement('header').classList.toggle('toggle');
-});
